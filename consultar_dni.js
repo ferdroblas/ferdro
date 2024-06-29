0296d7e6762fbe567d76639ecb6103be919b2c18
salbord <fernandodroblas@gmail.com>
2024-06-29 15:41:54 -0300

    Update consultar_dni.js

--- consultar_dni.js
+++ consultar_dni.js
@@ -1,14 +1,14 @@
-const config = require('./config'); // Ruta al archivo config.js
+const config = require('./config.js'); // Ruta al archivo config.js
 
 // Ahora puedes acceder a tu apiKey usando config.apiKey
-//const api_Key = config.apiKey;
+const llave = config.apiKey;
 
 gapi.load('client:auth2', initClient);
 
 function initClient() {
   gapi.client.init({
     // API Key para acceso público
-    apiKey: config.apiKey,
+    apiKey: llave,
     clientId: '521200181277-ggt79gnbpmco2qfl320pab9emrulr70m.apps.googleusercontent.com',
     // Descubrimiento de la API de Sheets
     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest'],
